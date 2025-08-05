3682d4aa6f32589580157cc5a9df633175aa783b
Dmitrii <glele@LAPTOP-GJATDHDQ.(none)>
2025-08-05 20:53:49 +0300

    HW2 part-1

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-from fastapi import FastAPI
+import json
+import uvicorn
+
+from fastapi import FastAPI, HTTPException
+from http import HTTPStatus
+from models.User import User
 
 app = FastAPI()
 
-@app.get("/")
-def main():
-    return "Hello world"
+users: list[User]
 
 
-#@app.get("/status")
-#def main():
-#    return "{status:ok}"
+@app.get("/api/users/{user_id}", status_code=HTTPStatus.OK)
+def get_user(user_id: int) -> User:
+    if user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail="Invalid user_id")
+    if user_id > len(users):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
+    return users[user_id - 1]
+
+
+@app.get("/api/users/", status_code=HTTPStatus.OK)
+def get_users() -> list[User]:
+    return users
 
 
 if __name__ == "__main__":
-    import uvicorn
+    with open("users.json") as f:
+        users = json.load(f)
+
+    for user in users:
+        User.model_validate(user)
+
+    print('Users loaded')
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
